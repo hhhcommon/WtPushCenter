e5ee4fb230cf23305a285f1d521471f4e9080e5b
wanghui <wanghui_whwx@sina.com>
2017-03-21 11:32:21 +0800

    修改sh启动命令

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-#启动推送服务
 echo start push
 _heapMem="-Xms512m -Xmx1024m"
-#java $_heapMem -jar PushCenter.jar
-nohup java $_heapMem -jar PushCenter.jar > push.log 2>&1 &
+nohup java $_heapMem -jar PushCenter.jar > ./logs/push.log 2>&1 &
